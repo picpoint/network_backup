0ffebdd50f418ee8b9dfc1492d30a2290b329093
picpoint <picpoint@yandex.ru>
2024-07-29 09:06:26 +0300

    work with copy files

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+import shutil
+import os
+
+FOLDER_SRC = "src"
+FOLDER_DST = "dst"
+
+def copy_files(src_folder, dst_folder):
+    shutil.copy2(f"{FOLDER_SRC}\\16.jpg", f"{FOLDER_DST}")
+    list_files = os.listdir(src_folder)
+
+    for i in list_files:
+        print(i)
+
+
+
+copy_files(FOLDER_SRC, FOLDER_DST)
