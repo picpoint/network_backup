1ed380790c111c1fdb99c92246d858745844005f
picpoint <picpoint@yandex.ru>
2024-08-21 16:03:31 +0300

    work with date of arhivs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import paramiko
 from passwd import access_server_backup
 from passwd import access_server_arhivs
+import datetime
 
 
 hostname_server_backup = access_server_arhivs['hostname']
@@ -13,7 +14,19 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname_server_backup, username=user_server_backup, password=password_server_backup)
 
+current_dt = datetime.date.today()
+current_date = current_dt.strftime("%d-%m-%Y")
+print(f"Current date - {current_date}")
+
 name_of_file = "02_02_2024_full_b1_s1_v1.tib"
+less_name = name_of_file[:-18]
+arhiv_name = less_name.replace('_', "-")
+print(f"Arhiv date - {arhiv_name}")
+
+if arhiv_name == current_date:
+    print("Arhiv is find")
+else:
+    print("arhive is not exists")
 
 '''
 копирование файла
@@ -33,9 +46,9 @@ name_of_file = "02_02_2024_full_b1_s1_v1.tib"
 '''
 выполнение команд на удалённом сервере
 '''
-stdin, stdout, stderr = client.exec_command("cd ..; cd ..; cd /data2; pwd")
+# stdin, stdout, stderr = client.exec_command("cd ..; cd ..; cd /data2; pwd")
 # path_to_directory = stdout
-print(stdout.read().decode())
+# print(stdout.read().decode())
 
 
 '''
